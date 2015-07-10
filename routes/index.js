b0a920ec27321399e2e2d6a4bf6c005236f3137f
Paulina Winkowska <paulina.winkowska@stazysta.comarch.pl>
2015-07-10 13:14:06 +0200

    Simple webpage

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = router;
+var mongo = require('mongojs');
+var db = mongo('shop');
+var coll  = db.collection('products');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/products', function(req, res, next) {
+    coll.find(function(err, data) {
+        if(err){return next(err);}
+
+        res.json(data);
+    });
+
+});

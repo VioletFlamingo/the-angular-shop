cfac46d9c1a3d030c32a1a6e6f13055a77739b48
Paulina Winkowska <paulina.winkowska@stazysta.comarch.pl>
2015-07-13 11:32:51 +0200

    Title change

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var coll  = db.collection('products');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SHOP' });
 });
 
 router.get('/products', function(req, res, next) {
